94407f091ebbc6224d8bae12dc5d687503a7e8dc
marcelrojas <marcelrojas2k@gmail.com>
2026-03-19 18:45:41 -0400

    Mixed

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,51 @@
 'use strict';
+
+const CACHE_NAME = 'marcelrojas-cache-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/manifest.webmanifest',
+    '/favicon.svg',
+    '/fonts/InterVariable.woff2',
+    '/fonts/TrajanPro-Regular.ttf',
+    '/fonts/GeistMono.woff2'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                .filter((name) => name !== CACHE_NAME)
+                .map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            // Retorna de caché si existe, si no, hace la petición a la red
+            return cachedResponse || fetch(event.request).then((response) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        }).catch(() => {
+            // Offline page
+        })
+    );
+});
