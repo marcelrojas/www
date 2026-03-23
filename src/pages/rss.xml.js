422972b5aca1ca2cd3e41d1acb48fe620dadb222
marcelrojas <marcelrojas2k@gmail.com>
2026-03-23 16:26:19 -0400

    Fixes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,11 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '@types/consts';
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
+  const posts = await getCollection('weblog');
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.id}/`,
+      link: `/resources/${post.id}/`,
     })),
   });
 }
