cebb077a8f10f60dda91fd8c6e508b977b5a2b83
marcelrojas <marcelrojas2k@gmail.com>
2026-03-25 19:08:03 -0400

    Mixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,11 +4,11 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const posts = await getCollection('weblog');
   return rss({
+    title: 'Marcel Rojas\'s blog',
+    description: 'Thoughts on code, design, and passion for everything. Latest posts and updates',
+    site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      title: 'Marcel Rojas\'s blog',
-      description: 'Thoughts on code, design, and passion for everything. Latest posts and updates',
-      site: context.site,
       link: `/resources/${post.id}/`,
     })),
   });
