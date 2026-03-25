141d32b77512ca47b432c4bb3ae8f7f8488befe8
marcelrojas <marcelrojas2k@gmail.com>
2026-03-25 18:07:16 -0400

    Mixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,5 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '@types/consts';
 
 export async function GET(context) {
   const posts = await getCollection('weblog');
