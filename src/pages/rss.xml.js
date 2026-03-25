d7d4b37bbabf3b2420c8b87172bbe710a6080059
marcelrojas <marcelrojas2k@gmail.com>
2026-03-25 19:27:19 -0400

    Mixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,6 +7,7 @@ export async function GET(context) {
     title: 'Marcel Rojas\'s blog',
     description: 'Thoughts on code, design, and passion for everything. Latest posts and updates',
     site: context.site,
+    trailingSlash: false,
     items: posts.map((post) => ({
       ...post.data,
       link: `/resources/${post.id}/`,
