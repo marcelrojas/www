29f9f0b8e83a067b1b2d152b9a7926692b4e88bd
marcelrojas <marcelrojas2k@gmail.com>
2026-06-12 23:37:06 -0400

    Mixed

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,9 @@
 
 export default new Map([
-["src/content/weblog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fweblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")],
 ["src/content/showcase/decibellion.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fshowcase%2Fdecibellion.mdx&astroContentModuleFlag=true")],
 ["src/content/showcase/listen-harder-audio.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fshowcase%2Flisten-harder-audio.mdx&astroContentModuleFlag=true")],
-["src/content/showcase/world-wide-stereo-storefront.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fshowcase%2Fworld-wide-stereo-storefront.mdx&astroContentModuleFlag=true")]]);
+["src/content/showcase/panacousticon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fshowcase%2Fpanacousticon.mdx&astroContentModuleFlag=true")],
+["src/content/showcase/prodigal-ventures.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fshowcase%2Fprodigal-ventures.mdx&astroContentModuleFlag=true")],
+["src/content/showcase/world-wide-stereo-storefront.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fshowcase%2Fworld-wide-stereo-storefront.mdx&astroContentModuleFlag=true")],
+["src/content/weblog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fweblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")]]);
 		
